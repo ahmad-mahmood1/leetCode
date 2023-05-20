57e0899020c255099dda292f1a9738ce908ef09b
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2023-05-21 00:56:47 +0500

    refactor next smaller element  question

--- Stack/nextSmallerElement.py
+++ Stack/nextSmallerElement.py
@@ -1,16 +1,16 @@
 def nextSmallerElement(arr):
     stack = [-1]
     for i in range(len(arr) - 1, -1, -1):
-        while stack[-1] >= arr[i]:
+        while stack[-1] != -1 and stack[-1] >= arr[i]:
             stack.pop()
-
-        stack.append(arr[i])
-
-        arr[i] = stack[-2]
+        
+        item = arr[i]    
+        arr[i] = stack[-1]
+        stack.append(item)
 
     return arr
 
 
-arr = [3,3,1,1]
+arr = [3, 8, 5, 2, 25]
 
 print(nextSmallerElement(arr))
