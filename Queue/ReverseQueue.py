c63688d7da2b6b01373752ad26e1af262b5253a7
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2023-05-27 19:06:36 +0500

    reverse a queue

---
+++ Queue/ReverseQueue.py
@@ -0,0 +1,28 @@
+from queue import Queue
+from collections import deque
+
+
+def rev(q):
+    # add code here
+    stack = []
+    while not q.empty():
+        item = q.get()
+        print(item)
+        stack.append(item)
+    print(stack)
+    while len(stack):
+        q.put(stack[-1])
+        stack.pop()
+    return q
+
+
+queue = Queue(maxsize=5)
+
+queue.put(1)
+queue.put(2)
+queue.put(3)
+queue.put(4)
+queue.put(5)
+
+ans = rev(queue)
+print(list(ans.queue))
